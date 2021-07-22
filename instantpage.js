c63bf54a644ee34447c7e9566383295544f159d2
Jacob Groß <kurtextrem@gmail.com>
2021-07-22 13:01:01 +0200

    only execute prerender/prefetch when no scroll event occured

--- instantpage.js
+++ instantpage.js
@@ -1,13 +1,15 @@
-/*!instant.page5-(c)2019 Alexandre Dieulot;https://instant.page/license;modified by Jacob Gross*/
+/*!instant.page5.1-(c)2019 Alexandre Dieulot;https://instant.page/license;modified by Jacob Gross*/
 
 ;(function (document, location) {
 	'use strict'
 
-	let urlToPreload = ''
+	const preloadedUrls = []
 	let mouseoverTimer = 0
 	let lastTouchTimestamp = 0
 
 	const prefetcher = document.createElement('link')
+	if (!('closest' in prefetcher)) return // Safari 13.0.5 on iOS 13.3.1 on iPhone
+
 	let relList = prefetcher.relList
 	const supports = relList !== undefined && relList.supports !== undefined
 	const isPrerenderSupported = supports && relList.supports('prerender')
@@ -16,75 +18,85 @@
 	const DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION = 1111
 
 	let dataset = document.body.dataset
-	const mousedownShortcut = 'instantMousedownShortcut' in dataset
+	const mousedownShortcut = 'instantMousedown' in dataset
 	const allowQueryString = 'instantAllowQueryString' in dataset
 	const allowExternalLinks = 'instantAllowExternalLinks' in dataset
 	const has3G = navigator.connection !== undefined && navigator.connection.effectiveType.includes('3g')
-	const saveData = navigator.connection !== undefined && (navigator.connection.saveData || navigator.connection.effectiveType.includes('2g'))
+	const saveData =
+		navigator.connection !== undefined && (navigator.connection.saveData || navigator.connection.effectiveType.includes('2g'))
 
 	document.head.appendChild(prefetcher)
 
-	const HOVER_DELAY = ('instantIntensity' in dataset) ? +dataset.instantIntensity : 65
-	const useViewport = !saveData && ('instantViewport' in dataset) && 
+	const HOVER_DELAY = 'instantIntensity' in dataset ? +dataset.instantIntensity : 65
+	const useViewport =
+		!saveData &&
+		'instantViewport' in dataset &&
 		/* Biggest iPhone resolution (which we want): 414 × 896 = 370944
-     * Small 7" tablet resolution (which we don’t want): 600 × 1024 = 614400
-     * Note that the viewport (which we check here) is smaller than the resolution due to the UI’s chrome */
-		(('instantViewportMobile' in dataset) || document.documentElement.clientWidth * document.documentElement.clientHeight > 450000)
-	const prefetchLimit = has3G ? 1 : Infinity
+		 * Small 7" tablet resolution (which we don’t want): 600 × 1024 = 614400
+		 * Note that the viewport (which we check here) is smaller than the resolution due to the UI’s chrome */
+		('instantViewportMobile' in dataset || document.documentElement.clientWidth * document.documentElement.clientHeight > 450000)
+	const PREFETCH_LIMIT = !has3G ? ('instantAllowExternalLinks' in dataset ? +dataset.instantLimit : 1 / 0) : 1 // Infinity
 
 	document.addEventListener('touchstart', touchstartListener, { capture: true, passive: true })
 	document.addEventListener('mouseover', mouseoverListener, { capture: true })
 
-	if (mousedownShortcut)
-		document.addEventListener('mousedown', mousedownShortcutListener, { capture: true })
-	if (isPrerenderSupported)
-		document.addEventListener('mousedown', mousedownListener, { capture: true })
+	if (mousedownShortcut) document.addEventListener('mousedown', mousedownShortcutListener, { capture: true })
+	if (isPrerenderSupported) document.addEventListener('mousedown', mousedownListener, { capture: true }) // after 'mousedown' it leaves us ~80ms prerender time to mouseup.
 
 	// @todo Add prefetchLimit & test if multiple prefetches work on on rel element
-	if (useViewport) {
+	if (useViewport && window.IntersectionObserver && 'isIntersecting' in IntersectionObserverEntry.prototype) {
 		// https://www.andreaverlicchi.eu/quicklink-optimal-options/
-		const SCROLL_DELAY = ('instantScrollDelay' in dataset) ? +dataset.instantScrollDelay : 500
+		const SCROLL_DELAY = 'instantScrollDelay' in dataset ? +dataset.instantScrollDelay : 500
 		const THRESHOLD = 0.75
 
-    const triggeringFunction = (callback) => {
-        requestIdleCallback(callback, {
-          timeout: 1500,
-        })
-      }
+		const triggeringFunction = callback => {
+			requestIdleCallback(callback, {
+				timeout: 1500,
+			})
+		}
 
 		const hrefsInViewport = []
-    
-    triggeringFunction(() => {
-      const intersectionObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-					const linkElement = entry.target
-          if (entry.isIntersecting) {
-						// Adding href to array of hrefsInViewport
-						hrefsInViewport.push(linkElement.href)
 
-						setTimeout(() => {
-							// Do not prefetch if not found in viewport
-							if (hrefsInViewport.indexOf(linkElement.href) === -1) return
+		triggeringFunction(() => {
+			const intersectionObserver = new IntersectionObserver(
+				entries => {
+					for (let i = 0; i < entries.length; ++i) {
+						const entry = entries[i]
+						const linkElement = entry.target
 
-							intersectionObserver.unobserve(linkElement)
-							preload(linkElement.href)
-						}, SCROLL_DELAY)
-          } else {
-						const index = hrefsInViewport.indexOf(linkElement.href)
-						if (index > -1) {
-							hrefsInViewport.splice(index)
+						if (hrefsInViewport.length > PREFETCH_LIMIT) return
+
+						if (entry.isIntersecting) {
+							// Adding href to array of hrefsInViewport
+							hrefsInViewport.push(linkElement.href)
+
+							setTimeout(() => {
+								// Do not prefetch if not found in viewport
+								if (hrefsInViewport.indexOf(linkElement.href) === -1) return
+
+								intersectionObserver.unobserve(linkElement)
+								preload(linkElement.href, false, true)
+							}, SCROLL_DELAY)
+						} else {
+							const index = hrefsInViewport.indexOf(linkElement.href)
+							if (index !== -1) {
+								hrefsInViewport.splice(index)
+							}
 						}
 					}
-        })
-      }, { threshold: THRESHOLD })
+				},
+				{ threshold: THRESHOLD }
+			)
 
-      document.querySelectorAll('a').forEach((linkElement) => {
-        if (isPreloadable(linkElement)) {
-          intersectionObserver.observe(linkElement)
-        }
-      })
-    },)
-  }
+			const nodes = document.querySelectorAll('a')
+			for (let i = 0; i < nodes.length; ++i) {
+				const node = nodes[i]
+				if (isPreloadable(node)) {
+					intersectionObserver.observe(node)
+				}
+			}
+		})
+	}
 
 	dataset = relList = null // GC
 
@@ -94,70 +106,53 @@
 	 * @param {{ target: { closest: (arg0: string) => any; }; }} event
 	 */
 	function touchstartListener(event) {
-		if (!('closest' in event.target)) return // Safari 13.0.5 on iOS 13.3.1 on iPhone
-
 		isMobile = true
-		const passive = { passive: true }
 
 		/* Chrome on Android calls mouseover before touchcancel so `lastTouchTimestamp`
 		 * must be assigned on touchstart to be measured on mouseover. */
 		lastTouchTimestamp = Date.now()
 
 		const linkElement = event.target.closest('a')
-
 		if (!isPreloadable(linkElement)) return
 
-		linkElement.addEventListener('touchcancel', touchendAndTouchcancelListener, passive)
-		linkElement.addEventListener('touchend', touchendAndTouchcancelListener, passive)
+		window.addEventListener('scroll', mouseoutListener, { once: true }) // if a scroll occurs before HOVER_DELAY, user is scrolling around
 
-		urlToPreload = linkElement.href
-		preload(linkElement.href)
-	}
-
-	function touchendAndTouchcancelListener() {
-		urlToPreload = ''
-		stopPreloading()
+		mouseoverTimer = setTimeout(mouseoverTimeout.bind(undefined, linkElement, true), HOVER_DELAY)
 	}
 
 	/**
 	 * @param {{ target: { closest: (arg0: string) => any; }; }} event
 	 */
 	function mouseoverListener(event) {
-		if (!('closest' in event.target)) return // Safari 13.0.5 on iOS 13.3.1 on iPhone
 		if (Date.now() - lastTouchTimestamp < DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION) return
 
 		const linkElement = event.target.closest('a')
-
 		if (!isPreloadable(linkElement)) return
 
-		linkElement.addEventListener('mouseout', mouseoutListener, {
-			passive: true,
-		})
+		linkElement.addEventListener('mouseout', mouseoutListener)
 
-		urlToPreload = linkElement.href
+		mouseoverTimer = setTimeout(mouseoverTimeout.bind(undefined, linkElement, false), HOVER_DELAY)
+	}
 
-		mouseoverTimer = setTimeout(
-			function () {
-				preload(linkElement.href)
-				mouseoverTimer = 0
-			},
-			HOVER_DELAY
-		)
+	function mouseoverTimeout(linkElement, important) {
+		if (isPrerenderSupported && isMobile) prerender(linkElement.href)
+		else preload(linkElement.href, important, !(isMobile && (saveData || has3G))) // on mobile we want to cancel requests when data saver is enabled or user has slow connection
+		mouseoverTimer = undefined
 	}
 
 	/**
 	 * @param {{ relatedTarget: { closest: (arg0: string) => any; }; target: { closest: (arg0: string) => any; }; }} event
 	 */
 	function mouseoutListener(event) {
-		if (event.relatedTarget && (!('closest' in event.target) || event.target.closest('a') === event.relatedTarget.closest('a'))) return
+		const target = event.target
+		if (event.relatedTarget && (!('closest' in target) || target.closest('a') === event.relatedTarget.closest('a'))) return
 
 		if (mouseoverTimer) {
 			clearTimeout(mouseoverTimer)
-			mouseoverTimer = 0
+			mouseoverTimer = undefined
 			return
 		}
 
-		urlToPreload = ''
 		stopPreloading()
 	}
 
@@ -170,7 +165,7 @@
 		if (event.which > 1 || event.metaKey || event.ctrlKey) return
 
 		const linkElement = event.target.closest('a')
-		if (!isPreloadable(linkElement)) return
+		if (linkElement === null || 'noInstant' in linkElement.dataset || linkElement.getAttribute('download') !== null) return // we don't use isPreloadable because this might lead to external links
 
 		linkElement.addEventListener(
 			'click',
@@ -190,19 +185,20 @@
 		if (event.which > 1 || event.metaKey || event.ctrlKey) return
 
 		const linkElement = event.target.closest('a')
-		if (!isPreloadable(linkElement)) return
+		if (!isPreloadable(linkElement, true)) return
 
-		prerender(linkElement.href) 
+		prerender(linkElement.href)
 	}
 
 	/**
 	 * @param {HTMLElement} linkElement
+	 * @param ignoreUrlCheck
 	 */
-	function isPreloadable(linkElement) {
+	function isPreloadable(linkElement, ignoreUrlCheck) {
 		let href
 		if (linkElement === null || !(href = linkElement.href)) return false
 
-		if (urlToPreload === href || href.charCodeAt(0) === 35 /* # */) return false
+		if ((!ignoreUrlCheck && preloadedUrls.indexOf(href) !== -1) || href.charCodeAt(0) === 35 /* # */) return false
 
 		const preloadLocation = new URL(href)
 
@@ -220,36 +216,62 @@
 
 	/**
 	 * @param {string} url
+	 * @param important
+	 * @param newTag
 	 */
-	function _prefetch(url, important) {
-		//console.log('prefetch', url)
-		prefetcher.rel = 'prefetch'
-		prefetcher.href = url
-		if (importannt) prefetcher.importance = 'high'
+	function _prefetch(url, important, newTag) {
+		console.log('prefetch', url)
+		preloadedUrls.push(url)
+
+		let fetcher = prefetcher
+		if (newTag) {
+			fetcher = document.createElement('link')
+			document.head.appendChild(fetcher)
+		}
+
+		fetcher.rel = 'prefetch'
+		fetcher.href = url
+		if (important) fetcher.setAttribute('importance', 'high')
 	}
+
+	let speculationTag
 
 	/**
 	 * @param {string} url
 	 */
 	function prerender(url) {
-		//console.log('prerender', url)
+		console.log('prerender', url)
 		prefetcher.rel = 'prerender'
 		prefetcher.href = url
 
 		// https://docs.google.com/document/d/1P2VKCLpmnNm_cRAjUeE-bqLL0bslL_zKqiNeCzNom_w/edit
 		if (isMobile) {
-			const tag = document.createElement('script')
-			tag.src = 'speculationrules'
+			if (!speculationTag) {
+				speculationTag = document.createElement('script')
+				speculationTag.src = 'speculationrules'
+				document.head.appendChild(speculationTag)
+			}
+
 			const obj = { prerender: [{ source: 'list', urls: [url] }] }
-			tag.textContent = JSON.stringify(obj)
+			speculationTag.textContent = JSON.stringify(obj)
 		}
 	}
 
 	/**
 	 * @param {string} url
+	 * @param important
+	 * @param newTag
 	 */
-	function _preload(url) {
-		//console.log('preload', url)
+	function _preload(url, important, newTag) {
+		console.log('preload', url)
+		preloadedUrls.push(url)
+
+		let fetcher = prefetcher
+		if (newTag) {
+			fetcher = document.createElement('link')
+			document.head.appendChild(fetcher)
+		}
+
 		prefetcher.rel = 'preload'
 		prefetcher.as = 'document'
 		prefetcher.href = url
