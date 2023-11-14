cd9dd9747923ef3e91ffe185e31464486c9d1f41
Jacob Groß <kurtextrem@gmail.com>
2023-11-14 16:37:41 +0100

    update to latest instant.page & fix `viewport`

--- instantpage.js
+++ instantpage.js
@@ -1,11 +1,36 @@
-/*!instant.page5.1-(c)2019 Alexandre Dieulot;https://instant.page/license;modified by Jacob Gross*/
+/*!instant.page5.2-(c)2019-23 Alexandre Dieulot;https://instant.page/license;modified by Jacob Gross*/
 
 ;(function (document, location, Date) {
 	'use strict'
 
 	if (!('Set' in window)) return
 	// min browsers: Edge 15, Firefox 54, Chrome 51, Safari 10, Opera 38, Safari Mobile 10
-	
+
+	const handleVaryAcceptHeader = 'instantVaryAccept' in document.body.dataset || 'Shopify' in window
+	// The `Vary: Accept` header when received in Chromium 79–109 makes prefetches
+	// unusable, as Chromium used to send a different `Accept` header.
+	// It’s applied on all Shopify sites by default, as Shopify is very popular
+	// and is the main source of this problem.
+	// `window.Shopify` only exists on “classic” Shopify sites. Those using
+	// Hydrogen (Remix SPA) aren’t concerned.
+
+	let _chromiumMajorVersionInUserAgent
+
+	const chromiumUserAgentIndex = navigator.userAgent.indexOf('Chrome/')
+	if (chromiumUserAgentIndex > -1) {
+		_chromiumMajorVersionInUserAgent = parseInt(navigator.userAgent.substring(chromiumUserAgentIndex + 'Chrome/'.length))
+	}
+	// The user agent client hints API is a theoretically more reliable way to
+	// get Chromium’s version… but it’s not available in Samsung Internet 20.
+	// It also requires a secure context, which would make debugging harder,
+	// and is only available in recent Chromium versions.
+	// In practice, Chromium browsers never shy from announcing "Chrome" in
+	// their regular user agent string, as that maximizes their compatibility.
+
+	if (handleVaryAcceptHeader && _chromiumMajorVersionInUserAgent && _chromiumMajorVersionInUserAgent < 110) {
+		return
+	}
+
 	const prefetcher = document.createElement('link')
 	const head = document.head
 	head.appendChild(prefetcher)
@@ -19,7 +44,9 @@
 	let relList = prefetcher.relList
 	const supports = relList !== undefined && relList.supports !== undefined // need this check, as Edge < 17, Safari < 10.1, Safari Mobile < 10.3 don't support this
 	const isPrerenderSupported = supports && relList.supports('prerender')
-	const preload = !supports || relList.supports('prefetch') ? _prefetch : relList.supports('preload') ? _preload : () => {} // Safari (11.1, mobile 11.3) only supports preload; for other browser we prefer prefetch over preload
+	const preload = !supports || relList.supports('prefetch') ? _prefetch : relList.supports('preload') ? _preload : false // Safari (11.1, mobile 11.3) only supports preload; for other browser we prefer prefetch over preload
+
+	if (!preload && !isPrerenderSupported) return // nothing we can do for the browser.
 
 	const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || {}
 	const effectiveType = typeof connection.effectiveType === 'string' ? connection.effectiveType : ''
@@ -27,49 +54,38 @@
 	const saveData = connection.saveData || effectiveType.indexOf('2g') !== -1
 
 	let dataset = document.body.dataset
-	const mousedownShortcut = 'instantMousedown' in dataset
+	const mousedownShortcut = 'instantMousedownShortcut' in dataset
 	const allowQueryString = 'instantAllowQueryString' in dataset
 	const allowExternalLinks = 'instantAllowExternalLinks' in dataset
-	const allowSpeculationRules = !('instantNoSpeculation' in dataset) && HTMLScriptElement.supports && HTMLScriptElement.supports('speculationrules')
-	
-	let chromiumMajorVersionClientHint = null
-	// `navigator.userAgentData` is available in Chromium 90+,
-	// though it was not enabled for everyone at first.
-	// So it’s only reliable for Chromium ~100+, and only on HTTPS or localhost.
-	if (navigator.userAgentData) {
-  		navigator.userAgentData.brands.forEach(function(obj) {
-    			if (obj.brand == 'Chromium') {
-      				chromiumMajorVersionClientHint = +obj.version
-    			}
-  		})
-	}
-	
+	const allowSpeculationRules =
+		!('instantNoSpeculation' in dataset) && HTMLScriptElement.supports && HTMLScriptElement.supports('speculationrules')
+	const useWhitelist = 'instantWhitelist' in document.body.dataset
+
 	const useViewport =
 		!saveData &&
-		'instantViewport' in dataset &&
-		// Smartphones are the most likely to have a slow connection, and
-          	// their small screen size limits the number of links (and thus
-          	// server load).
-          	//
-          	// Foldable phones (being expensive as of 2023), tablets and PCs
-          	// generally have a decent connection, and a big screen displaying
-          	// more links that would put more load on the server.
-          	//
-          	// iPhone 14 Pro Max (want): 430×932 = 400 760
-          	// Samsung Galaxy S22 Ultra with display size set to 80% (want):
-          	// 450×965 = 434 250
-          	// Small tablet (don’t want): 600×960 = 576 000
-          	// Those number are virtual screen size, the viewport (used for
-          	// the check above) will be smaller with the browser’s interface.
-		('instantViewportMobile' in dataset || document.documentElement.clientWidth * document.documentElement.clientHeight > 450000)
+		('instantViewport' in dataset ||
+			// Smartphones are the most likely to have a slow connection, and
+			// their small screen size limits the number of links (and thus
+			// server load).
+			//
+			// Foldable phones (being expensive as of 2023), tablets and PCs
+			// generally have a decent connection, and a big screen displaying
+			// more links that would put more load on the server.
+			//
+			// iPhone 14 Pro Max (want): 430×932 = 400 760
+			// Samsung Galaxy S22 Ultra with display size set to 80% (want):
+			// 450×965 = 434 250
+			// Small tablet (don’t want): 600×960 = 576 000
+			// Those number are virtual screen size, the viewport (used for
+			// the check above) will be smaller with the browser’s interface.
+			('instantViewportMobile' in dataset && document.documentElement.clientWidth * document.documentElement.clientHeight < 450000))
 
 	const DELAY_TO_NOT_BE_CONSIDERED_A_TOUCH_INITIATED_ACTION = 1111
 	const HOVER_DELAY = 'instantIntensity' in dataset ? +dataset.instantIntensity : 65
 
 	// only trigger `prefetch` requests on mobile, as ~90ms is too slow to get a preload done on mobile;
 	// we need to do this, as prefetch + speculationrules do not share the same request (so we might end up with 2-3 reqs)
-	if (preload !== _preload)
-		document.addEventListener('touchstart', touchstartListener, { capture: true, passive: true })
+	if (preload !== _preload) document.addEventListener('touchstart', touchstartListener, { capture: true, passive: true })
 
 	let listenerOptions = { capture: true }
 	document.addEventListener('mouseover', mouseoverListener, listenerOptions)
@@ -78,10 +94,11 @@
 	if (isPrerenderSupported) document.addEventListener('mousedown', mousedownListener, listenerOptions) // after 'mousedown' it leaves us ~80ms prerender time to mouseup.
 
 	if (useViewport && window.IntersectionObserver && 'isIntersecting' in IntersectionObserverEntry.prototype) {
-		// https://www.andreaverlicchi.eu/quicklink-optimal-options/
+		// https://verlok.github.io/quicklink-optimal-options/
 		const PREFETCH_LIMIT = !has3G ? (allowExternalLinks ? +dataset.instantLimit : 1 / 0) : 1 // Infinity
 		const SCROLL_DELAY = 'instantScrollDelay' in dataset ? +dataset.instantScrollDelay : 500
-		const THRESHOLD = 0.75
+		const THRESHOLD = 'instantThreshold' in dataset ? +dataset.instantThreshold : 0.9
+		const SELECTOR = 'instantSelector' in dataset ? dataset.instantSelector : 'a'
 
 		const triggeringFunction = callback => {
 			requestIdleCallback(callback, {
@@ -121,7 +138,7 @@
 				{ threshold: THRESHOLD }
 			)
 
-			const nodes = document.querySelectorAll('a')
+			const nodes = document.querySelectorAll(SELECTOR)
 			for (let i = 0; i < nodes.length; ++i) {
 				const node = nodes[i]
 				if (isPreloadable(node)) {
@@ -136,7 +153,7 @@
 	let isMobile = false
 
 	function checkForClosestAnchor(event, relatedTarget) {
-		const target = !relatedTarget? event.target : event.relatedTarget
+		const target = !relatedTarget ? event.target : event.relatedTarget
 		if (!target || typeof target.closest !== 'function') return
 
 		return target.closest('a')
@@ -242,15 +259,15 @@
 		const preloadLocation = new URL(href)
 
 		if (linkElement.origin != location.origin) {
-    			let allowed = allowExternalLinks || 'instant' in linkElement.dataset
-    			if (!allowed || !chromiumMajorVersionClientHint) {
-      				return false
-    			}
-  		}
+			let allowed = allowExternalLinks || 'instant' in linkElement.dataset
+			if (!allowed || !_chromiumMajorVersionInUserAgent) {
+				return false
+			}
+		}
 
 		if (preloadLocation.protocol !== 'http:' && preloadLocation.protocol !== 'https:') return false
 		if (preloadLocation.protocol === 'http:' && location.protocol === 'https:') return false
-		if (!allowQueryString && preloadLocation.search && !('instant' in linkElement.dataset)) return false
+		if ((useWhitelist || (!allowQueryString && preloadLocation.search)) && !('instant' in linkElement.dataset)) return false
 		if (preloadLocation.hash && preloadLocation.pathname + preloadLocation.search === location.pathname + location.search) return false
 		if ('noInstant' in linkElement.dataset) return false
 		if (linkElement.getAttribute('download') !== null) return false
@@ -283,9 +300,9 @@
 		fetcher.href = url
 		fetcher.rel = 'prefetch'
 		fetcher.as = 'document'
-  		// as=document is Chromium-only and allows cross-origin prefetches to be
-  		// usable for navigation. They call it “restrictive prefetch” and intend
-  		// to remove it: https://crbug.com/1352371
+		// as=document is Chromium-only and allows cross-origin prefetches to be
+		// usable for navigation. They call it “restrictive prefetch” and intend
+		// to remove it: https://crbug.com/1352371
 
 		if (newTag) head.appendChild(fetcher)
 	}
@@ -301,14 +318,15 @@
 
 		// trigger PrerenderV2: https://chromestatus.com/feature/5197044678393856
 		// Before Chromium 107, adding another speculation tag instead of modifying the existing one fails
-		if (allowSpeculationRules) { // && chromiumMajorVersionClientHint >= 107; but this check would exclude other Chromium browsers (Opera) that support this.
+		if (allowSpeculationRules) {
+			// && chromiumMajorVersionClientHint >= 107; but this check would exclude other Chromium browsers (Opera) that support this.
 			const speculationTag = document.createElement('script')
 			speculationTag.textContent = JSON.stringify({ prerender: [{ source: 'list', urls: [url] }] })
 			speculationTag.type = 'speculationrules'
 			head.appendChild(speculationTag)
-			return;
+			return
 		}
-		
+
 		if (important) prefetcher.setAttribute('fetchPriority', 'high')
 		prefetcher.href = url
 		prefetcher.rel = 'prerender'
@@ -320,9 +338,10 @@
 	 * @param newTag
 	 */
 	function _preload(url, important, newTag) {
-		if (isMobile) // ~90ms is too slow to get a preload done on mobile (request can not be re-used)
-			return;
-		
+		if (isMobile)
+			// ~90ms is too slow to get a preload done on mobile (request can not be re-used)
+			return
+
 		console.log('preload', url)
 
 		preloadedUrls.add(url)
